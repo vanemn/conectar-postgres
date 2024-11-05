89a91b18adf4710b2ab3493ecc2271ad78c12d9b
Vane <vane.moralesn@gmail.com>
2024-11-05 20:46:56 -0300

    ajuste de linea en models.py

--- desafiodb/testdb/models.py
+++ desafiodb/testdb/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 class AdlTest(models.Model):
     campo1 = models.CharField(max_length=100)
     valor1 = models.IntegerField()
